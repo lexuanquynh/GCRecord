6834fa97d71cb5651eb2cd362d40a8c05c37f485
Brank <le-xuan-quynh@goodcycle.net>
2024-10-03 11:57:20 +0700

    Initial Commit

---
+++ GCRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCRecord
+//
+//  Created by Le Xuan Quynh on 3/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GCRecordTests/GCRecordTests.swift
@@ -0,0 +1,17 @@
+//
+//  GCRecordTests.swift
+//  GCRecordTests
+//
+//  Created by Le Xuan Quynh on 3/10/24.
+//
+
+import Testing
+@testable import GCRecord
+
+struct GCRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
