c10c27ae94d728b0a673c5ec45911c9b9af13841
Brank <le-xuan-quynh@goodcycle.net>
2024-10-04 07:36:00 +0700

    first commit

---
+++ GCRecord/RecordScreen/Cell/RecordAudioTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordAudioTableViewCell.swift
+//  GCRecord
+//
+//  Created by Le Xuan Quynh on 3/10/24.
+//
+
+import UIKit
+
+class RecordAudioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GCRecord/RecordScreen/RecordAudioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecordAudioViewController.swift
+//  GCRecord
+//
+//  Created by Le Xuan Quynh on 3/10/24.
+//
+
+import UIKit
+
+class RecordAudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
